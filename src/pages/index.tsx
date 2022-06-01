078a35e1860b0f7f464afc9141c88a453a65930c
gojaebeom <const.gjb@gmail.com>
2022-06-01 22:37:54 +0900

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,15 @@ import { isBrowser } from "../utils/isBrower"
 
 export default function HomePage({ data }) {
   const [contentIsLoaded, setContentIsLoaded] = useState(false)
+
+  let filter = ""
+  if (isBrowser) {
+    const queryString = window.location.href.split("?category=")[1]
+    if (queryString !== undefined) {
+      console.debug(queryString)
+      filter = queryString
+    }
+  }
   useEffect(() => {
     if (isBrowser) {
       import("../utils/images").then(({ imgProcess }) => {
@@ -25,31 +34,41 @@ export default function HomePage({ data }) {
       <div className="w-full masonry-layout">
         {data.allMdx.nodes.map(node => {
           return (
-            <Link to={`/blog/${node.frontmatter.slug}`} key={node.id}>
-              <article className="w-1/2 md:w-[33%] lg:w-[25%] xl:w-[20%] p-2 masonry-item mb-4">
-                <div className="w-full overflow-hidden rounded-xl">
-                  <img src={node.frontmatter.thumbnail} loading="lazy" />
+            <article
+              className="w-1/2 md:w-[33%] lg:w-[25%] xl:w-[20%] p-2 masonry-item mb-4"
+              key={node.id}
+            >
+              <Link
+                to={`/blog/${node.frontmatter.slug}`}
+                className="flex justify-center items-center w-full overflow-hidden rounded-xl"
+              >
+                <img src={node.frontmatter.thumbnail} loading="lazy" />
+              </Link>
+              <div className="p-2">
+                <div className="text-sm lg:text-md">
+                  <i className="mr-1 fa-light fa-timer"></i>
+                  <span className="font-noto-l">{node.frontmatter.date}</span>
                 </div>
-                <div className="p-2">
-                  <div className="text-sm lg:text-md">
-                    <i className="mr-1 fa-light fa-timer"></i>
-                    <span className="font-noto-l">{node.frontmatter.date}</span>
-                  </div>
-                  <div className="text-lg lg:text-xl font-noto-b2">
-                    {node.frontmatter.title}
-                  </div>
-                  <div className="text-xs lg:text-sm">
-                    {node.frontmatter.excerpt}
-                  </div>
-                  <div className="flex flex-wrap justify-start">
-                    <span className="flex items-center justify-center text-xs font-jetbrain-l font-semibold text-blue-500">
-                      <i className="fa-light fa-hashtag mr-0.5"></i>
-                      <span>{node.frontmatter.category}</span>
-                    </span>
-                  </div>
+                <Link
+                  to={`/blog/${node.frontmatter.slug}`}
+                  className="text-lg lg:text-xl font-noto-b2"
+                >
+                  {node.frontmatter.title}
+                </Link>
+                <div className="text-xs lg:text-sm">
+                  {node.frontmatter.excerpt}
                 </div>
-              </article>
-            </Link>
+                <div className="flex flex-wrap justify-start">
+                  <Link
+                    to={`/?category=${node.frontmatter.category}`}
+                    className="flex items-center justify-center text-xs font-jetbrain-l font-semibold text-blue-500"
+                  >
+                    <i className="fa-light fa-hashtag mr-0.5"></i>
+                    <span>{node.frontmatter.category}</span>
+                  </Link>
+                </div>
+              </div>
+            </article>
           )
         })}
       </div>
